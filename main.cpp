b9f79f2a81dd4f7c3c49b04ddfd88f0277442a19
adanilishin <adanilishin@WIN-ARFHPLSLFR9>
2024-06-30 02:21:08 +0300

    added glass and water

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ int main() {
 
     auto material_ground = make_shared<Lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<Lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto matetial_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left = make_shared<dielectric>(1.50);
+    auto matetial_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_ground));
     world.add(make_shared<sphere>(point3(0,0,-1.2), 0.5, material_center));
@@ -36,4 +36,4 @@ int main() {
     cam.render(world);
 }
 
-// next: Fuzzy Reflection
+// next: Schlick Approximation
