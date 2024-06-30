a4f3b9564e2b710fad56e73e5d615d9d4b104f2e
adanilishin <adanilishin@WIN-ARFHPLSLFR9>
2024-06-30 17:35:43 +0300

    added images

--- main.cpp
+++ main.cpp
@@ -15,12 +15,14 @@ int main() {
 
     auto material_ground = make_shared<Lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<Lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<dielectric>(1.50);
+    auto material_left   = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     auto matetial_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100, material_ground));
     world.add(make_shared<sphere>(point3(0,0,-1.2), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,0.0,-1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
     world.add(make_shared<sphere>(point3(1.0,0.0,-1.0), 0.5, matetial_right));
 
     // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
@@ -29,11 +31,11 @@ int main() {
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 10;
+    cam.image_width = 1000;
+    cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.render(world);
 }
 
-// next: Schlick Approximation
+// next: Positionable Camera
